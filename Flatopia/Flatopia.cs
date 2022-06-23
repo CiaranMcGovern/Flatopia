ecefe45b6a7e35f5a6a5931e22f5102dd7bb2834
Ciaran McGovern <ciaranmcgovern@mail.com>
2022-06-23 23:53:11 +0100

    Fix Game Files

---
+++ Flatopia/Flatopia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flatopia.FlatopiaEngine;
+
+namespace Flatopia
+{
+    public class Flatopia : FlatopiaEngine.FlatopiaEngine
+    {
+        public Flatopia() : base(new FlatopiaEngine.Vector2(615, 515), "Flatopia")
+        {
+
+        }
+
+        public override void OnLoad()
+        {
+            Console.WriteLine("Onload works");
+        }
+    }
+}
---
+++ Flatopia/FlatopiaEngine/FlatopiaEngine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+
+namespace Flatopia.FlatopiaEngine
+{
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+
+    public abstract class FlatopiaEngine
+    {
+        private Vector2 ScreenDimensions = new Vector2(512, 512);
+        private string Title = "New Game";
+        private Canvas Window = null;
+        private Thread GameLoopThread = null;
+
+        public FlatopiaEngine(Vector2 screenDimensions, string title)
+        {
+            this.ScreenDimensions = screenDimensions;
+            this.Title = title;
+
+            Window = new Canvas();
+            Window.Size = new Size((int)ScreenDimensions.X, (int)ScreenDimensions.Y);
+            Window.Text = title;
+            Window.Paint += Renderer;
+
+            OnLoad();
+            GameLoopThread = new Thread(GameLoop);
+            GameLoopThread.Start();
+
+            Application.Run(Window);
+        }
+        void GameLoop()
+        {
+            while (GameLoopThread.IsAlive)
+            {
+
+            }
+        }
+
+        private void Renderer(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+        }
+
+        public abstract void OnLoad();
+
+    }
+}
